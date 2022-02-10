3c36b828cc50cdec823470d2dafcb6645205380b
João Reis <joaoreisjpk@gmail.com>
2022-02-10 14:15:41 -0300

    updating the toggleReminder method to fit on json-server logic

--- src/App.vue
+++ src/App.vue
@@ -55,16 +55,29 @@ export default {
       if (confirm("Are you sure?")) {
         const res = await fetch(`api/tasks/${id}`, { method: "DELETE" });
 
-        res.status === 200 ? this.tasks = this.tasks.filter((task) => {
-          return task.id !== id;
-        }) : alert('Error deleting task')
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => {
+              return task.id !== id;
+            }))
+          : alert("Error deleting task");
       }
     },
     async toggleReminder(id) {
-      const newArray = [...this.tasks];
+      const taskToToggle = await this.fetchTask(id);
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-      this.tasks = newArray.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      });
+
+      const data = await res.json();
+
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
     async fetchTasks() {
